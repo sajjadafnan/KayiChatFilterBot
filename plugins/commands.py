1cc3ec8ecbc142da5e3b87e8ce5aa757e7e5717f
sajjadafnan <71611618+sajjadafnan@users.noreply.github.com>
2021-04-04 12:57:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -264,10 +264,6 @@ async def about(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
-                ],
-                [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("CLOSE", callback_data="close_data"),
                 ]                
