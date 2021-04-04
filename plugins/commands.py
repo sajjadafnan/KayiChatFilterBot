a54eb53082db8aa80f836c83d02a46d298b7544d
sajjadafnan <71611618+sajjadafnan@users.noreply.github.com>
2021-04-04 13:04:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,8 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
-                ],
-                [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("⭕️ Join Channel", url="https://t.me/EE_Links"),
+                    InlineKeyboardButton("⭕️ Join Group", url="https://t.me/KayiChat")
                 ]
             ]
         ),
